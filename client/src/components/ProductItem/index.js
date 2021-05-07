d8a8da88d0088d8f6f44b5d2ae42251d00547109
MalekBenJemia <malekbenjemia@gmail.com>
2021-05-06 22:25:14 -0400

    4

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,3 +1,5 @@
+import { idbPromise } from "../../utils/helpers";
+
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers";
@@ -17,24 +19,26 @@ function ProductItem(item) {
 
   const { cart } = state;
 
-const addToCart = () => {
-  // find the cart item with the matching id
-  const itemInCart = cart.find((cartItem) => cartItem._id === _id);
-
-  // if there was a match, call UPDATE with a new purchase quantity
-  if (itemInCart) {
-    dispatch({
-      type: UPDATE_CART_QUANTITY,
-      _id: _id,
-      purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-    });
-  } else {
-    dispatch({
-      type: ADD_TO_CART,
-      product: { ...item, purchaseQuantity: 1 }
-    });
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...item, purchaseQuantity: 1 }
+      });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+    }
   }
-};
 
   return (
     <div className="card px-1 py-1">
